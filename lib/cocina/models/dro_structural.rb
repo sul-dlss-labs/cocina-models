503547e8d2b42b7f32b09d19c0e55a80fc8d3700
Justin Coyne <jcoyne@justincoyne.com>
2021-05-07 16:18:28 -0500

    Move hasAgreement to AdminPolicy
    Fixes #259

--- lib/cocina/models/dro_structural.rb
+++ lib/cocina/models/dro_structural.rb
@@ -6,8 +6,6 @@ module Cocina
       attribute :contains, Types::Strict::Array.of(FileSet).meta(omittable: true)
       attribute :hasMemberOrders, Types::Strict::Array.of(Sequence).meta(omittable: true)
       attribute :isMemberOf, Types::Strict::Array.of(Druid).meta(omittable: true)
-      # Agreement that covers the deposit of the DRO into SDR.
-      attribute :hasAgreement, Types::Strict::String.meta(omittable: true)
     end
   end
 end
